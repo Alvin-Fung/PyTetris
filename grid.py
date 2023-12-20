5f0fc2a0d330f592a86882c3af9ccd425eca72f8
Alvin-Fung <alvin_fung9@outlook.com>
2023-12-20 17:08:07 +0000

    Created temporary background colour for testing and created grid class

---
+++ grid.py
@@ -0,0 +1,9 @@
+class Grid:
+    def __init__(self):
+        self.num_rows = 20 
+        self.num_cols = 10
+        self.cell_size = 30
+        # Array to represent the grid itself
+        self.grid = [[0 for i in range(self.num.cols)] for j in range (self.num_rows)]
+
+
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pygame
 import random
 import sys
 
-# Initial setup
+# Game loop
 pygame.init()
+dark_blue = (44, 44, 127)
 screen = pygame.display.set_mode((300, 600)) # Width, Height
 pygame.display.set_caption("Alvin's Tetris made in Python!")
 clock = pygame.time.Clock()
@@ -11,14 +12,18 @@ running = True
 
 while running:
     for event in pygame.event.get():
+        # pygame.QUIT event allows the user to click X to close the game window.
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
-
+    # Game Rendering/Drawing
+    screen.fill(dark_blue)
+    
+    pygame.display.update()
+    clock.tick(60) # Limits the game to run at 60 FPS.
+    
 # Global Variables
 blockSize = 30
-
-# Colours 
 
 # Shape Formats
 S = [[
