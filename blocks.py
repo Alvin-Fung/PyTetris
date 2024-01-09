f8ea0d2b12bb7d296a90a0bfc5ca9007a600889d
Alvin-Fung <alvin_fung9@outlook.com>
2024-01-09 17:07:23 +0000

    Found out the positions states needed. I still don't get it right now haha

--- blocks.py
+++ blocks.py
@@ -7,7 +7,7 @@ class LBlock(Block):
         self.cells = {
             #Rotation State 0, ...,...
             0: [Position(0,2), Position(1,0), Position(1,1), Position(1,2)],
-            1: [Position(0,1), Position(0,1), Position(0,1), Position(0,1)],
-            2: [Position(0,1), Position(0,1), Position(0,1), Position(0,1)],
-            3: [Position(0,1), Position(0,1), Position(0,1), Position(0,1)]
+            1: [Position(0,1), Position(1,1), Position(2,1), Position(2,2)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
+            3: [Position(0,0), Position(0,1), Position(1,1), Position(2,1)]
         }
