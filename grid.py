fcd15c8025671a63eb1ef076b629fe8be7de75be
Alvin-Fung <alvin_fung9@outlook.com>
2024-01-09 16:39:29 +0000

    Got rid of local method for getting colours and imported colors from Colors

--- grid.py
+++ grid.py
@@ -1,4 +1,5 @@
 import pygame
+from colors import Colors
 
 class Grid:
     def __init__(self):
@@ -7,7 +8,7 @@ class Grid:
         self.cell_size = 30
         # Array to represent the grid itself
         self.grid = [[0 for i in range(self.num_cols)] for j in range (self.num_rows)]
-        self.colors = self.get_cell_colors()
+        self.colors = Colors.get_cell_colors()
 
     def print_grid(self):
         for row in range(self.num_rows):
@@ -15,18 +16,8 @@ class Grid:
                 print(self.grid[row][column], end = " ")
             print()
             
-    def get_cell_colors(self):
-        
-        dark_grey = (26, 31, 40)
-        red = (232, 18, 18)
-        green = (47, 230, 23)
-        blue = (13, 64, 216)
-        orange = (226, 116, 17)
-        yellow = (237, 234, 4)
-        purple = (166, 0, 247)
-        cyan = (21, 204, 209)
-        
-        return [dark_grey, red, green, blue, orange, yellow, purple, cyan]
+    
+            
 
     def draw(self, screen):
         for row in range(self.num_rows):
