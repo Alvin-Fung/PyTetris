cd9ddf3a298fac7f9e3ed9cd69e070eb1ab42a4c
Alvin-Fung <alvin_fung9@outlook.com>
2024-01-09 16:54:30 +0000

    Created child class blocks, got to do the positions

--- block.py
+++ block.py
@@ -1,7 +1,12 @@
+from colors import Colors
+
 class Block:
     def __init__(self):
         self.id = id
         self.cells = {}
         self.cell_size = 30
         self.rotation_state = 0
-         
+        self.colors = Colors.get_cell_colors()
+    
+    def draw(self, screen):
+        pass
