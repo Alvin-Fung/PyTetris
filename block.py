957cebaef7448364c23169571df32b40fa49c073
Alvin-Fung <alvin_fung9@outlook.com>
2024-01-16 11:56:06 +0000

    Created row and column offset positions to move the blocks and created move method based on those offsets

--- block.py
+++ block.py
@@ -1,13 +1,21 @@
 from colors import Colors
 import pygame
+from position import Position
 
 class Block:
     def __init__(self, id):
         self.id = id
         self.cells = {}
         self.cell_size = 30
+        self.row_offset = 0
+        self.column_offset = 0
         self.rotation_state = 0
         self.colors = Colors.get_cell_colors()
+        
+    def move(self, rows, columns):
+        self.row_offset += rows
+        self.column_offset += columns
+        
     
     def draw(self, screen):
         tiles = self.cells[self.rotation_state] #Retrieves the current position of the rotation state
