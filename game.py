d998e10471bce184f717ddb4f96a0e2593ba8a16
Alvin-Fung <alvin_fung9@outlook.com>
2024-01-16 15:53:42 +0000

    hmm something is wrong with my code

--- game.py
+++ game.py
@@ -11,12 +11,21 @@ class Game:
         
     #Randomly decides what block is used next
     def get_random_block(self):
-        if self.blocks == 0:
+        if len(self.blocks) == 0:
             self.blocks = [IBlock(), JBlock(), LBlock(), SBlock(), TBlock(), ZBlock()]
         block = random.choice(self.blocks)
         self.blocks.remove(block)
         return block
-        
+
+    def move_left(self):
+        self.current_block.move(0, -1)
+    
+    def move_right(self):
+        self.current_block.move(0, 1)
+    
+    def move_down(self):
+        self.current_block.move(1 , 0)
+    
     def draw(self, screen):
         self.grid.draw(screen)
         self.current_block.draw(screen)
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame,sys
-from game import game
+from game import Game
 
 # Game loop
 pygame.init()
 dark_blue = (44, 44, 127)
+
 screen = pygame.display.set_mode((300, 600)) # Width, Height
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
 running = True
+
+game = Game()
 
 while running:
     for event in pygame.event.get():
@@ -16,8 +19,17 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_left()
+            if event.key == pygame.K_RIGHT:
+                game.move_right()
+            if event.key == pygame.K_DOWN:
+                game.move_down()
+            
     # Game Rendering/Drawing
     screen.fill(dark_blue)
+    game.draw(screen)
     
     pygame.display.update()
     clock.tick(60) # Limits the game to run at 60 FPS.
