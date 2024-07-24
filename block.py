f4cac18545a3c9d090d650a372db6473175c3505
Alvin-Fung <alvin_fung9@outlook.com>
2024-07-24 15:54:06 +0100

    Created undo_rotation()
    Needs a similar if statement like rotate() except I would probably need to check if the length of the state would be 0 in order to decrement

--- block.py
+++ block.py
@@ -31,6 +31,10 @@ class Block:
         if self.rotation_state == len(self.cells): # This resets the rotation state after the final increment
             self.rotation_state = 0
     
+    # Similar to rotate(), this will undo the rotation if the block is outside of the grid
+    def undo_rotation(self):
+        self.rotation -= 1
+        
     def draw(self, screen):
         tiles = self.get_cell_positions() #Retrieves the current position of the rotation state
         for tile in tiles:
