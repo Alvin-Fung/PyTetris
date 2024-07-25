1d73a9cc13bd6cb26ca8dfd05ad512bf273818b0
Alvin-Fung <alvin_fung9@outlook.com>
2024-07-25 17:13:27 +0100

    Should also do this for rotation as well

--- game.py
+++ game.py
@@ -48,7 +48,7 @@ class Game:
                 
     def rotate(self):
         self.current_block.rotate()
-        if self.block_inside == False:
+        if self.block_inside == False or self.block_fits() == False:
             self.current_block.undo_rotation()
     
     def block_inside(self):
