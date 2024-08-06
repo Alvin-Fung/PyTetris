635affd3adc70732f96a037a8c649a1aaa4d9b22
Alvin-Fung <alvin_fung9@outlook.com>
2024-08-06 12:15:39 +0100

    Added Next block and game over render

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ pygame.init()
 
 title_font = pygame.font.Font(None, 40)
 score_surface = title_font.render("Score", True, Colors.white)
+next_surface = title_font.render("Next", True, Colors.white)
+game_over_surface = title_font.render("Game Over!", True, Colors.white)
 
 score_rect = pygame.Rect(320, 50, 170, 60)
+next_rect = pygame.Rect(320, 215, 170, 180)
 
 screen = pygame.display.set_mode((500, 620)) # Width, Height
 pygame.display.set_caption("Python Tetris")
@@ -51,7 +54,12 @@ while running:
     # Game Rendering/Drawing
     screen.fill(Colors.dark_blue)
     screen.blit(score_surface, (365, 20, 50, 50))
+    screen.blit(next_surface, (375, 180, 50, 50))
+    if game.game_over == True:
+        screen.blit(game_over_surface, (320, 450, 50, 50))
+    
     pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10) # 0 and 10 to have rounded corners
+    pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
     game.draw(screen)
     
     pygame.display.update()
